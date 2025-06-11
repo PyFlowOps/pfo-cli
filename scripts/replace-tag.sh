2d0c453f5b5e33f1899dc6f94a1a3bc263aa6cb0
Philip De Lorenzo <philip.delorenzo@gmail.com>
2025-06-10 21:44:46 -0700

    chore: add replace tag script

---
+++ scripts/replace-tag.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# We want to make some change to the code, but keep the same tag for the version
+RELEASE_TAG="v0.0.2"
+
+git tag --delete "${RELEASE_TAG}"
+git push --delete origin "${RELEASE_TAG}"
+
+# Now we can push the new tag
+git tag -a "${RELEASE_TAG}" -m "Release: ${RELEASE_TAG} -- SRE Automated Release/Tagging"
+git push origin "${RELEASE_TAG}"
